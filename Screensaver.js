78537dbd74bdd17aba49fb4e8df1aa289c118b15
Samisauur <60164690+Samisauur@users.noreply.github.com>
2020-02-16 00:00:44 -0700

    Update Screensaver.js

--- Screensaver.js
+++ Screensaver.js
@@ -1,6 +1,233 @@
-// First Screen Saver SS1
-basic.forever(function () {
+//Outline for screensavers assignment
 
+let awake: boolean = true
+let gest: Gesture = Gesture.Shake
+let gestures = [Gesture.Shake, Gesture.LogoDown, Gesture.TiltLeft, Gesture.TiltRight, Gesture.ThreeG]
+let foos = [ss1, ss2, ss3, ss4, ss5]
+
+
+
+input.onButtonPressed(Button.A, function () {
+    awake = true
+})
+
+input.onButtonPressed(Button.B, function () {
+    awake = false
+})
+input.onGesture(Gesture.Shake, function () {
+    gest = Gesture.Shake
+})
+
+input.onGesture(Gesture.LogoDown, function () {
+    gest = Gesture.LogoDown
+})
+
+input.onGesture(Gesture.TiltLeft, function () {
+    gest = Gesture.TiltLeft
+})
+
+input.onGesture(Gesture.TiltRight, function () {
+    gest = Gesture.TiltRight
+})
+input.onGesture(Gesture.ThreeG, function () {
+    gest = Gesture.ThreeG
+})
+
+
+function ss1() {
+    for (let i = 0; i < 1; i++) {
+        led.plot(2, 2)
+        led.plot(0, 0)
+        led.plot(1, 1)
+        led.plot(2, 2)
+        led.plot(3, 3)
+        led.plot(4, 4)
+        basic.pause(80)
+        basic.clearScreen()
+        led.plot(2, 2)
+    }
+    for (let i = 0; i < 1; i++) {
+        led.plot(2, 2)
+        led.plot(2, 0)
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        basic.pause(80)
+        basic.clearScreen()
+        led.plot(2, 2)
+    }
+    for (let i = 0; i < 1; i++) {
+        led.plot(2, 2)
+        led.plot(4, 0)
+        led.plot(3, 1)
+        led.plot(2, 2)
+        led.plot(1, 3)
+        led.plot(0, 4)
+        basic.pause(80)
+        basic.clearScreen()
+        led.plot(2, 2)
+    }
+    for (let i = 0; i < 1; i++) {
+        led.plot(2, 2)
+        led.plot(0, 2)
+        led.plot(1, 2)
+        led.plot(2, 2)
+        led.plot(3, 2)
+        led.plot(4, 2)
+        basic.pause(80)
+        basic.clearScreen()
+        led.plot(2, 2)
+    
+    }
+
+}
+
+function ss2() {
+    let bar1 = game.createSprite(0, 0)
+    let bar2 = game.createSprite(0, 1)
+    let bar3 = game.createSprite(0, 2)
+    let bar4 = game.createSprite(0, 3)
+    let bar5 = game.createSprite(0, 4)
+
+    let timing: number[] = [50, 55, 60, 65]
+
+
+    for (let i = 0; (i) < 2; i++) {
+        for (let j = 0; j < 2; j++) {
+            bar1.move(1)
+            bar1.ifOnEdgeBounce()
+            basic.pause(timing[j])
+
+        }
+    }
+    for (let i = 0; (i) < 2; i++) {
+        for (let j = 0; j < 2; j++) {
+            bar2.move(1)
+            bar2.ifOnEdgeBounce()
+            basic.pause(timing[j])
+
+        }
+    }
+    for (let i = 0; (i) < 2; i++) {
+        for (let j = 0; j < 2; j++) {
+            bar3.move(1)
+            bar3.ifOnEdgeBounce()
+            basic.pause(timing[j])
+
+        }
+    }
+    for (let i = 0; (i) < 2; i++) {
+        for (let j = 0; j < 2; j++) {
+            bar4.move(1)
+            bar4.ifOnEdgeBounce()
+            basic.pause(timing[j])
+
+        }
+    }
+    for (let i = 0; (i) < 1; i++) {
+        for (let j = 0; j < 4; j++) {
+            bar5.move(1)
+            bar5.ifOnEdgeBounce()
+            basic.pause(timing[j])
+
+        }
+        basic.pause(100)
+
+
+
+    }
+    bar1.delete()
+    bar2.delete()
+    bar3.delete()
+    bar4.delete()
+    bar5.delete()
+}
+
+function ss3() {
+    basic.plotLeds(`
+    . . . . .
+    # # . . .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . .
+    # # # . .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . .
+    # # . # .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . #
+    # # . . .
+    # # . # .
+    # . . . .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . .
+    # # . . .
+    # # . . #
+    # . # . .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . .
+    # # # . .
+    # # . . .
+    # . . # .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . .
+    # # . # .
+    # # . . .
+    # . . . #
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . #
+    # # . . .
+    # # # . .
+    # . . . .
+    # . . . .
+    `)
+    basic.plotLeds(`
+    . . . . .
+    # # . # .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+    basic.pause(100)
+    basic.plotLeds(`
+    . . . . #
+    # # . . .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+    basic.pause(100)
+
+}
+
+function ss4() {
     for (let i = 0; i < 5; i++) {
         led.plotBrightness(i, i, 100)
         led.plot(0, i)
@@ -9,174 +236,21 @@ basic.forever(function () {
         led.plot(i, 2)
         led.plot(i, 0)
         led.plot(i, 4)
-        basic.pause(100)
+        basic.pause(400)
         basic.clearScreen()
-    }
-})
-// Second Screen Saver SS2
-basic.forever(function () {
-    for (let i = 0; i < 1; i++) {
-        led.plot(2, 2)
-        led.plotBrightness(0, 0, 80)
-        led.plotBrightness(1, 1, 160)
-        led.plotBrightness(2, 2, 255)
-        led.plotBrightness(3, 3, 160)
-        led.plotBrightness(4, 4, 80)
-        basic.pause(80)
-        basic.clearScreen()
-        led.plot(2, 2)
-    }
-    for (let i = 0; i < 1; i++) {
-        led.plot(2, 2)
-        led.plotBrightness(2, 0, 80)
-        led.plotBrightness(2, 1, 160)
-        led.plotBrightness(2, 2, 255)
-        led.plotBrightness(2, 3, 160)
-        led.plotBrightness(2, 4, 80)
-        basic.pause(80)
-        basic.clearScreen()
-        led.plot(2, 2)
-    }
-    for (let i = 0; i < 1; i++) {
-        led.plot(2, 2)
-        led.plotBrightness(4, 0, 80)
-        led.plotBrightness(3, 1, 160)
-        led.plotBrightness(2, 2, 255)
-        led.plotBrightness(1, 3, 160)
-        led.plotBrightness(0, 4, 80)
-        basic.pause(80)
-        basic.clearScreen()
-        led.plot(2, 2)
-    }
-    for (let i = 0; i < 1; i++) {
-        led.plot(2, 2)
-        led.plotBrightness(0, 2, 80)
-        led.plotBrightness(1, 2, 160)
-        led.plotBrightness(2, 2, 255)
-        led.plotBrightness(3, 2, 160)
-        led.plotBrightness(4, 2, 80)
-        basic.pause(80)
-        basic.clearScreen()
-        led.plot(2, 2)
-    }
-})    
 
-// third screen saver SS3
-//////////////////////////////////////////////
-///////////////////////////////////////
-//////////////////////////////////////////////
-////////////////////////////////////////////
-////////////////////////////////////
-// fourth screen saver ss4
-let bar1 = game.createSprite(0, 0)
-let bar2 = game.createSprite(0, 1)
-let bar3 = game.createSprite(0, 2)
-let bar4 = game.createSprite(0, 3)
-let bar5 = game.createSprite(0, 4)
-let lumocity: number[] = [255, 200, 160, 110, 60]
-let timing: number[] = [50, 55, 60, 65]
+    }
+}
+function ss5() {
+
+}
+
 
 basic.forever(function () {
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            bar1.move(1)
-            bar1.ifOnEdgeBounce()
-            basic.pause(timing[j])
-            bar1.set(LedSpriteProperty.Brightness, lumocity[bar1.get(LedSpriteProperty.Y)]);
-        }
+    if (awake) {
+        basic.showString("Hallo")
+    } else {  // screensavers
+        foos[gestures.indexOf(gest)]()
     }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            bar2.move(1)
-            bar2.ifOnEdgeBounce()
-            basic.pause(timing[j])
-            bar2.set(LedSpriteProperty.Brightness, lumocity[bar2.get(LedSpriteProperty.Y)]);
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            bar3.move(1)
-            bar3.ifOnEdgeBounce()
-            basic.pause(timing[j])
-            bar3.set(LedSpriteProperty.Brightness, lumocity[bar3.get(LedSpriteProperty.Y)]);
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            bar4.move(1)
-            bar4.ifOnEdgeBounce()
-            basic.pause(timing[j])
-            bar4.set(LedSpriteProperty.Brightness, lumocity[bar4.get(LedSpriteProperty.Y)]);
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            bar5.move(1)
-            bar5.ifOnEdgeBounce()
-            basic.pause(timing[j])
-            bar5.set(LedSpriteProperty.Brightness, lumocity[bar5.get(LedSpriteProperty.Y)]);
-        }
-    }
-})
-    
-// fifth screensaver ss5
-let muz0 = game.createSprite(0, 4)
-let muz1 = game.createSprite(1, 4)
-let muz2 = game.createSprite(2, 4)
-let muz3 = game.createSprite(3, 4)
-let muz4 = game.createSprite(4, 4)
-muz0.turn(Direction.Right, (-90))
-muz1.turn(Direction.Right, (-90))
-muz2.turn(Direction.Right, (-90))
-muz3.turn(Direction.Right, (-90))
-muz4.turn(Direction.Right, (-90))
-let lumes: number[] = [255, 185, 135, 85, 25];
-let timing: number[] = [30, 40, 60, 70]
 
-basic.forever(function () {
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 4; j++) {
-            muz0.set(LedSpriteProperty.Brightness, lumes[muz0.get(LedSpriteProperty.Y)]);
-            muz0.move(1)
-            muz0.ifOnEdgeBounce()
-            basic.pause(timing[j])
-
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 4; j++) {
-            muz1.set(LedSpriteProperty.Brightness, lumes[muz1.get(LedSpriteProperty.Y)]);
-            muz1.move(1)
-            muz1.ifOnEdgeBounce()
-            basic.pause(timing[j])
-
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 4; j++) {
-            muz2.set(LedSpriteProperty.Brightness, lumes[muz2.get(LedSpriteProperty.Y)]);
-            muz2.move(1)
-            muz2.ifOnEdgeBounce()
-            basic.pause(timing[j])
-
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 4; j++) {
-            muz3.set(LedSpriteProperty.Brightness, lumes[muz3.get(LedSpriteProperty.Y)]);
-            muz3.move(1)
-            muz3.ifOnEdgeBounce()
-            basic.pause(timing[j])
-
-        }
-    }
-    for (let i = 0; (i) < 2; i++) {
-        for (let j = 0; j < 4; j++) {
-            muz4.set(LedSpriteProperty.Brightness, lumes[muz4.get(LedSpriteProperty.Y)]);
-            muz4.move(1)
-            muz4.ifOnEdgeBounce()
-            basic.pause(timing[j])
-
-        }
-    }
 })
